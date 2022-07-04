f324d77a0c6d21fea55adca841db12a1e382c9af
IvanZezyukin <i.zezukin@gmail.com>
2022-07-04 20:13:02 +0300

    somehow commander.js added

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+//const { program } = require('commander');
+import { Command } from 'commander';
+const program = new Command();
+
+program
+    .option('-V, --version', 'output the version number')
+    .description('Compares two configuration files and shows a difference.');
+
+program.parse();
+
+// const options = program.opts();
+// const limit = options.first ? 1 : undefined;
+// console.log(program.args[0].split(options.separator, limit));
+
+if (program.version) {
+    console.log('Version 0.1');
+}
