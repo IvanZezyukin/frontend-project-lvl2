133497ce511aa61d9eb802249d428beb9e6ee855
IvanZezyukin <i.zezukin@gmail.com>
2022-07-05 08:47:02 +0300

    commander.js modification

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
-
+/*
 //const { program } = require('commander');
 import { Command } from 'commander';
 const program = new Command();
 
 program
+    .usage('Usage: gendiff [options] <filepath1> <filepath2>')
     .option('-V, --version', 'output the version number')
     .description('Compares two configuration files and shows a difference.');
 
@@ -17,3 +18,22 @@ program.parse();
 if (program.version) {
     console.log('Version 0.1');
 }
+ */
+
+import { program } from 'commander';
+
+const command = (filepath1, filepath2) => {
+    console.log('hello!' + filepath1 + filepath2);
+};
+
+program
+    .version('0.1')
+    .arguments('<filepath1>')
+    .arguments('<filepath2>')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
+    .action(command)
+    .parse(process.argv);
+
+const options = program.opts();
+if (options.format) console.log('format!' + options.format);
