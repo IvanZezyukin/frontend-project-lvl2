0b4752721101d04fc62c7f7c79167f4837794231
IvanZezyukin <i.zezukin@gmail.com>
2022-07-11 08:35:32 +0300

    fixtures and dirname added

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -1,14 +1,15 @@
 import index from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import fs from 'fs';
+import path from 'path';
 
-const result = `{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-test('first', () => {
-  expect(index('./files/file1.json', './files/file2.json')).toEqual(result);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('Flat json comparison', () => {
+  expect(index(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(readFile('result.json'));
 });
